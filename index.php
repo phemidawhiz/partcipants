c2736e73964d3a520ccbdf0b3e989191626bc85a
Ayodele Olufemi Segun <feminiscence@gmail.com>
2017-08-21 13:42:44 +0100

    Upload of index and css file

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Participants Page </title>
+
+    <meta name="keywords" content="Participants">
+    <meta name="description" content="collaborative project">
+    <meta name="author" content="Ayodele Olufemi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- page specific styles -->
+  	<link rel="stylesheet" href="style.css">
+
+    <!-- local lib -->
+    <link rel="stylesheet" href="css-lib/cutegrids.css">
+
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="search">
+        <form>
+          <input type="text" name="search" placeholder="Enter Participant name to search">
+        </form>
+      </div>
+
+      <div class="participants">
+        <div class="flex-container">
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="participants">
+        <div class="flex-container">
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="participants">
+        <div class="flex-container">
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="participants">
+        <div class="flex-container">
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item">
+            <div class="row">
+              <div class="cute-3-laptop" >
+                <img src="images/femi.png" alt="profile-picture" />
+              </div>
+              <div class="cute-6-laptop">
+                <a href="#"><b>Ayodele Olufemi </b></a><br><br>
+                <p>feminiscence@gmail.com</p>
+              </div>
+              <div class="cute-3-laptop">
+                <p>Team 3 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
